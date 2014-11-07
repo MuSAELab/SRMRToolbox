a353436eb17fed8060754ae1d00608109c5ec6f7
Joao Felipe Santos <joao.eel@gmail.com>
2014-11-07 14:53:48 -0500

    Updated test script to remove mention to startup.m

--- test/test_SRMR.m
+++ test/test_SRMR.m
@@ -1,4 +1,5 @@
-% Before using this test script, call startup.m to set up the path and compile the MEX extensions
+% Before using this test script, set up the path and compile the MEX extensions
+% as instructed in the README file.
 
 % First possible syntax: pass filename as argument, without sampling rate
 file = 'test.wav';
